4fb8f8c2ace93d950f24e4a7c0fb68283a04f646
Joshua Chung <87676426+jchu634@users.noreply.github.com>
2026-04-13 23:52:07 +1000

    Added Uniwind

---
+++ metro.config.js
@@ -0,0 +1,13 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+const { withUniwindConfig } = require("uniwind/metro");
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+module.exports = withUniwindConfig(config, {
+  cssEntryFile: "./app/global.css",
+  // (optional) path where we gonna auto-generate typings
+  // defaults to project's root
+  dtsFile: "./app/uniwind-types.d.ts",
+});
