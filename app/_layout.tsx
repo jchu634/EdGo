3eb940b69d28f4908015ea4dcfae7fe47ee35a16
Joshua Chung <87676426+jchu634@users.noreply.github.com>
2026-04-25 16:32:14 +1000

    Added safe area insets for android navbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,21 @@
+import { Platform } from "react-native";
 import { Stack } from "expo-router";
 import { SQLiteProvider } from "expo-sqlite";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { initDB } from "@/src/lib/courseStorage";
 
 export default function RootLayout() {
+  const insets = useSafeAreaInsets();
+
   return (
     <SQLiteProvider databaseName="edgo.db" onInit={initDB}>
-      <Stack />
+      <Stack
+        screenOptions={{
+          contentStyle: {
+            paddingBottom: Platform.OS === "android" ? insets.bottom : 0,
+          },
+        }}
+      />
     </SQLiteProvider>
   );
 }
