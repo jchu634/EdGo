f32adf2645a2622dc61d892eb71133ddb3c917c1
Joshua Chung <87676426+jchu634@users.noreply.github.com>
2026-04-27 21:08:58 +1000

    Started drizzle migration

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,7 @@ const { withUniwindConfig } = require("uniwind/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+config.resolver.sourceExts.push("sql");
 
 module.exports = withUniwindConfig(config, {
   cssEntryFile: "./app/global.css",
---
+++ src/db/drizzle.ts
@@ -0,0 +1,5 @@
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { openDatabaseSync } from "expo-sqlite";
+
+const expo = openDatabaseSync("db.db");
+const db = drizzle(expo);
