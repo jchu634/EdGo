72bc13e9a0db709c235443b01b49d7eb4a9cab85
Joshua Chung <87676426+jchu634@users.noreply.github.com>
2026-05-15 03:36:33 +1000

    Added settings page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator, Platform } from "react-native";
+import { ActivityIndicator, Platform, Pressable } from "react-native";
 import { Suspense } from "react";
 import { Stack } from "expo-router";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -7,9 +7,11 @@ import { Ionicons } from "@expo/vector-icons";
 import { KeyProvider } from "@/src/providers/keyProvider";
 import { DbProvider } from "@/src/providers/dbProvider";
 import { LinkTextProvider } from "@/src/components/LinkText";
+import { useRouter } from "expo-router";
 
 export default function RootLayout() {
   const insets = useSafeAreaInsets();
+  const router = useRouter();
 
   return (
     <Suspense fallback={<ActivityIndicator size="large" />}>
@@ -17,27 +19,30 @@ export default function RootLayout() {
         <DbProvider>
           <LinkTextProvider>
             <Stack
-            screenOptions={{
-              headerStyle: {
-                backgroundColor: "#70069e",
-              },
-              headerTitle: "",
-              headerRight: () => (
-                <>
-                  <Ionicons
-                    name="search"
-                    size={24}
-                    color="white"
-                    style={{ marginRight: 16 }}
-                  />
-                  <Ionicons name="person" size={24} color="white" />
-                </>
-              ),
-              contentStyle: {
-                paddingBottom: Platform.OS === "android" ? insets.bottom : 0,
-              },
-            }}
-          />
+              screenOptions={{
+                headerStyle: {
+                  backgroundColor: "#70069e",
+                },
+                headerTintColor: "white",
+                headerTitle: "",
+                headerRight: () => (
+                  <>
+                    <Ionicons
+                      name="search"
+                      size={24}
+                      color="white"
+                      style={{ marginRight: 16 }}
+                    />
+                    <Pressable onPress={() => router.navigate("/settings")}>
+                      <Ionicons name="person" size={24} color="white" />
+                    </Pressable>
+                  </>
+                ),
+                contentStyle: {
+                  paddingBottom: Platform.OS === "android" ? insets.bottom : 0,
+                },
+              }}
+            />
           </LinkTextProvider>
         </DbProvider>
       </KeyProvider>
---
+++ app/settings/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useCallback, useMemo } from "react";
+import { View, Text, Pressable } from "react-native";
+import { clearCourseCache, clearThreadCache } from "@/src/lib/storage";
+
+import "@/app/global.css";
+
+export default function Index() {
+  return (
+    <View className="flex h-full p-4">
+      <Text className="font-display-bold text-2xl">User Details</Text>
+      <Text className="font-display">PLACEHOLDER NAME</Text>
+      <Text className="font-display">PLACEHOLDER EMAIL</Text>
+      <Text className="font-display">PLACEHOLDER REGION</Text>
+
+      <View className="absolute bottom-4 left-4 gap-y-2">
+        <Pressable
+          className="rounded-lg bg-red-100 px-3 py-1.5"
+          onPress={() => {
+            clearCourseCache();
+          }}
+        >
+          <Text className="font-display text-xs text-red-700">
+            Purge Course Cache
+          </Text>
+        </Pressable>
+        <Pressable
+          className="rounded-lg bg-red-100 px-3 py-1.5"
+          onPress={() => clearThreadCache()}
+        >
+          <Text className="font-display text-xs text-red-700">
+            Purge Thread Cache
+          </Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
