82941c7d8a76372dbbe5d937a6530d5bf2cd5ff1
Joshua Chung <87676426+jchu634@users.noreply.github.com>
2026-05-18 03:43:44 +1000

    Minor fixes

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,22 +1,21 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
-import journal from './meta/_journal.json';
-import m0000 from './0000_slow_demogoblin.sql';
-import m0001 from './0001_massive_blackheart.sql';
-import m0002 from './0002_real_jigsaw.sql';
-import m0003 from './0003_puzzling_robin_chapel.sql';
-import m0004 from './0004_mushy_spacker_dave.sql';
-import m0005 from './0005_chubby_miek.sql';
+import journal from "./meta/_journal.json";
+import m0000 from "./0000_slow_demogoblin.sql";
+import m0001 from "./0001_massive_blackheart.sql";
+import m0002 from "./0002_real_jigsaw.sql";
+import m0003 from "./0003_puzzling_robin_chapel.sql";
+import m0004 from "./0004_mushy_spacker_dave.sql";
+import m0005 from "./0005_chubby_miek.sql";
 
-  export default {
-    journal,
-    migrations: {
-      m0000,
-m0001,
-m0002,
-m0003,
-m0004,
-m0005
-    }
-  }
-  
+export default {
+  journal,
+  migrations: {
+    m0000,
+    m0001,
+    m0002,
+    m0003,
+    m0004,
+    m0005,
+  },
+};
