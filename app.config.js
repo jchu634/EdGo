618e5ea27186f78575ad9a09c661ef294a34cdd9
Joshua Chung <87676426+jchu634@users.noreply.github.com>
2026-06-02 06:37:42 +1000

    Version bump

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ const IS_DEV = process.env.APP_VARIANT === "development";
 export default {
   name: IS_DEV ? "EdGo (Dev)" : "EdGo",
   slug: "EdGo",
-  version: "0.1.1",
+  version: "0.1.2",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "edgo",
